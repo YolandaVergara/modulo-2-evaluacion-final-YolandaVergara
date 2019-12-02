a3fbee4067b6d5b2d9a38434913d5245429a2bb0
Yolanda <yv.avila@gmail.com>
2019-12-02 10:13:29 +0100

    Add paintFilms to js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,21 +5,43 @@ const userSearch = document.querySelector('.js-input');
 const btn = document.querySelector('.js-btn');
 let films = [];
 
-function getServerData() {
+
+//
+function getServerData(ev) {
+  ev.preventDefault()
   fetch(
-    `http://api.tvmaze.com/search/shows?q=${userSearch}`
-  )
+      `http://api.tvmaze.com/search/shows?q=${userSearch.value}`
+    )
     .then(response => response.json())
     .then(function (serverData) {
-      films = serverData.show;
+      films = serverData;
+      paintFilms();
     })
     .catch(function (err) {
       console.log("Error al traer los datos del servidor", err);
     });
 }
 
-function paintResults(){
-    
+
+function paintFilms() {
+  let htmlCode = "<ul>";
+  for (const film of films) {
+
+    const defaultImage = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+
+    htmlCode += `<li class="">`;
+    htmlCode += `<h3 class="film__name">${film.show.name}</h3>`;
+
+    if (film.show.image !== null) {
+      let filmImage = film.show.image.medium;
+      htmlCode += `<img src="${filmImage}">`;
+    } else {
+      htmlCode += `<img src="${defaultImage}"`;
+    }
+    htmlCode += `</li>`;
+  }
+  htmlCode += '</ul>';
+  dataResults.innerHTML = htmlCode;
 }
 
 btn.addEventListener('click', getServerData);
