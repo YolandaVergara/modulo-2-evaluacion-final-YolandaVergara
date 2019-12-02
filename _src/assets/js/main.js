c60b42bd75b7b75ecc59d2a12d7ccce7f9bed668
Yolanda <yv.avila@gmail.com>
2019-12-02 23:24:11 +0100

    Add comments to JS structure

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -10,16 +10,18 @@ let films = [];
 //variable predefinida como array en la que guardamos los favoritos
 let favoriteFilms = [];
 
+//guardamos las películas guardadas en el array de favoritos para que las cargue posteriormente, las pasamos a formato JSON
 function setLocalStorage() {
   localStorage.setItem("favorite", JSON.stringify(favoriteFilms));
 }
-
+//definimos la función que nos recuperará el localstorage
 function getLocalStorage() {
   const localStorageFavoriteFilms = JSON.parse(localStorage.getItem("favorite"));
-
+ //en el caso de que al coger el json y parsearlo no esté vacío, lo volcamos en nuestro array de favoritos y lo pintamos en pantalla 
   if (localStorageFavoriteFilms !== null) {
     favoriteFilms = localStorageFavoriteFilms;
     paintFavorites();
+    //en el caso de que esté vacío, ejecutamos la función que nos trae los datos de búsqueda del servidor
   } else {
     getServerData();
   }
@@ -27,10 +29,11 @@ function getLocalStorage() {
 
 //FETCH
 function getServerData() {
-
+//hacemos la búsqueda en la API, usando los criterios de búsqueda de la documentación de la misma
   fetch(
-      `http://api.tvmaze.com/search/shows?q=${userSearch.value}`
-    )
+      `http://api.tvmaze.com/search/shows?q=${userSearchnp.value}`
+    )//al traernos los datos del servidor, los guardamos en un array para poder usar los datos de forma cómoda, posteriormente ejecutamos la función de recuperar el localstorage, lo pintamos en pantalla y ejecutamos la función que escucha las series al clickarlas para posteriormente añadirlas a favoritos
+
     .then(response => response.json())
     .then(function (serverData) {
       films = serverData;
@@ -46,24 +49,24 @@ function getServerData() {
 //FUNCION PINTAR RESULTADOS
 function paintFilms() {
   let htmlCode = '';
-
+//creamos un método que recorre los resultados mostrados y almacena en favoritos cen función de si el id se encuentra en el array o no 
   for (let i = 0; i < films.length; i++) {
     const favoriteIndex = favoriteFilms.findIndex(function (show, favoriteIndex) {
       return show.id === films[i].show.id;
     })
-
+//en el caso de que devuelva la posición del id y sea diferente a -1 lo marca como favodito
     const isFavorite = favoriteIndex !== -1;
 
     const defaultImage =
       'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
-
+//en este condicional añadimos la clase que se le pone en el caso de estar en el array de favoritos
     if (isFavorite) {
       htmlCode += `<li class="js-films js-film-item favorite-film" id=${films[i].show.id} >`;
     } else {
       htmlCode += `<li class="js-films js-film-item" id=${films[i].show.id} >`;
     }
     htmlCode += `<h3 class="js-films-name">${films[i].show.name}</h3>`;
-
+//en este condicional indicamos la imagen que muestra por defecto en caso de no existir
     if (films[i].show.image !== null) {
       let filmImage = films[i].show.image.medium;
       htmlCode += `<img src="${filmImage}">`;
@@ -79,6 +82,7 @@ function paintFilms() {
 function toogleFavorites(ev) {
   //metemos en una constante el id de los elementos seleccionados
   const clickedItem = parseInt(ev.currentTarget.id);
+  //metemos en una constante el valor del id que tiene el elemento escuchado, si es igual al id que hemos almacenado en clickedItem añade o elimina en el caso de no serlo del array de favoritos
   const favoriteIndex = favoriteFilms.findIndex(function (show, favoriteIndex) {
     return show.id === clickedItem
   })
@@ -94,6 +98,7 @@ function toogleFavorites(ev) {
       }
     }
   }
+  //posteriormente repintamos los favoritos, preparamos para otra búsqueda, repintamos los nuevos resultados y los almacenamos en localStorage
   paintFilms();
   listenFilms();
   paintFavorites();
@@ -123,7 +128,7 @@ function handler(ev) {
   ev.preventDefault();
   getServerData();
 }
-
+//definimos la función que va a escuchar todos los resultados de ls búsquedas para añadir a favoritos
 function listenFilms() {
   const filmItems = document.querySelectorAll('.js-film-item');
 
