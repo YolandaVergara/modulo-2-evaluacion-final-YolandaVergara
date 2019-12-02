17e8ea5553495d5c8d2981c8babccb795067e4f4
Yolanda <yv.avila@gmail.com>
2019-12-02 14:39:43 +0100

    add favorites to fav array

--- README.md
+++ README.md
@@ -2,15 +2,15 @@ Buscador de series:
 
 Pasos a realizar:
 
-      [ ] Usuario pide info a través del input
-      [ ] Escucho click en el botón de enviar
-      [ ] Recogemos los datos del input y los guardamos en una variable
-      [ ] Hacemos fetch para pedir los datos del servidor con el filtro de búsqueda del usuario
-      [ ] Parseo los datos recibidos
-      [ ] El servidor me devuelve la info
-      [ ] Pinto N resultados en el html
-            [ ] Pinto titulo
-            [ ] Pinto imagen y en el caso de no tener, poner por defecto https://via.placeholder.com/210x295/ffffff/666666/?
+      [x] Usuario pide info a través del input
+      [x] Escucho click en el botón de enviar
+      [x] Recogemos los datos del input y los guardamos en una variable
+      [x] Hacemos fetch para pedir los datos del servidor con el filtro de búsqueda del usuario
+      [x] Parseo los datos recibidos
+      [x] El servidor me devuelve la info
+      [x] Pinto N resultados en el html
+            [x] Pinto titulo
+            [x] Pinto imagen y en el caso de no tener, poner por defecto https://via.placeholder.com/210x295/ffffff/666666/?
 text=TV
       [ ] Usuario elige resultados favoritos y se almacenan en un nuevo array
       [ ] Cambiamos la clase cambiando fondo y fuente
--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,12 +1,26 @@
 'use strict';
-
+//variable donde voy a mostrar el contenido buscado
 const dataResults = document.querySelector('.js-container');
+//variable donde guardamos la búsqueda del usuario
 const userSearch = document.querySelector('.js-input');
+//variable que ejecuta la busqueda
 const btn = document.querySelector('.js-btn');
+//variable predefinida como array en la que almacenaremos los datos para después mostrarlos
 let films = [];
+//variable predefinida como array en la que guardamos los favoritos
+let favoriteFilms = [];
+
+// function setLocalStorage() {
+//   localStorage.setItem('favoriteFilms', JSON.stringify(films));
+// }
+
+// function getLocalStorage() {
 
 
-//
+// }
+
+
+//FETCH
 function getServerData(ev) {
   ev.preventDefault()
   fetch(
@@ -16,24 +30,30 @@ function getServerData(ev) {
     .then(function (serverData) {
       films = serverData;
       paintFilms();
+      listenFilms();
     })
     .catch(function (err) {
-      console.log("Error al traer los datos del servidor", err);
+      console.log('Error al traer los datos del servidor', err);
     });
 }
 
-
+//FUNCIÓN PINTAR RESULTADOS
 function paintFilms() {
-  let htmlCode = "<ul>";
-  for (const film of films) {
-
+  let htmlCode = '<ul>';
+  for (let i = 0; i < films.length; i++) {
+    const favoriteItem = favoriteFilms.indexOf(i);
+    const isFavorite = favoriteItem !== -1
     const defaultImage = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
 
-    htmlCode += `<li class="">`;
-    htmlCode += `<h3 class="film__name">${film.show.name}</h3>`;
+    if (isFavorite) {
+      htmlCode += `<li class="js-film-item favorite-film" id=${films[i].show.id}>`;
+    } else {
+      htmlCode += `<li class="js-film-item" id=${films[i].show.id}>`;
+    }
 
-    if (film.show.image !== null) {
-      let filmImage = film.show.image.medium;
+    htmlCode += `<h3 class="js-film-name">${films[i].show.name}</h3>`;
+    if (films[i].show.image !== null) {
+      let filmImage = films[i].show.image.medium;
       htmlCode += `<img src="${filmImage}">`;
     } else {
       htmlCode += `<img src="${defaultImage}"`;
@@ -42,6 +62,31 @@ function paintFilms() {
   }
   htmlCode += '</ul>';
   dataResults.innerHTML = htmlCode;
+}
+
+function toggleFavorites(ev) {
+  //metemos en una constante el id de los elementos seleccionados
+  const clickedItem = parseInt(ev.currentTarget.id);
+  //guardamos en otra constante la posición de los elementos clickados
+  const favoriteFilm = favoriteFilms.indexOf(clickedItem);
+  //en el caso de no encontrarla, le asigna un -1, por lo que sólo guardamos los que son diferentes a -1
+  const isFavorite = favoriteFilm !== -1;
+  if (isFavorite === true) {
+    favoriteFilms.splice(favoriteFilm, 1);
+  } else {
+    favoriteFilms.push(parseInt(ev.currentTarget.id));
+  }
+  paintFilms();
+  listenFilms();
+}
+
+
+
+function listenFilms() {
+  const filmsItems = document.querySelectorAll('.js-film-item');
+  for (const filmsItem of filmsItems) {
+    filmsItem.addEventListener('click', toggleFavorites);
+  }
 }
 
 btn.addEventListener('click', getServerData);
