e9984c93e915878418e8a64b91aaf570a7c9a7a3
Yolanda <yv.avila@gmail.com>
2019-12-02 09:21:31 +0100

    add fetch function

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log('>> Ready :)');
+const dataResults = document.querySelector('.js-container');
+const userSearch = document.querySelector('.js-input');
+const btn = document.querySelector('.js-btn');
+let films = [];
+
+function getServerData() {
+  fetch(
+    `http://api.tvmaze.com/search/shows?q=${userSearch}`
+  )
+    .then(response => response.json())
+    .then(function (serverData) {
+      films = serverData.show;
+    })
+    .catch(function (err) {
+      console.log("Error al traer los datos del servidor", err);
+    });
+}
+
+function paintResults(){
+    
+}
+
+btn.addEventListener('click', getServerData);
