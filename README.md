b244af299d1d3ca5bcba21a697075cdbc3127db8
Yolanda <yv.avila@gmail.com>
2019-12-03 08:08:21 +0100

    add readme file

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
-Buscador de series:
-
-Pasos a realizar:
-
-      [x] Usuario pide info a través del input
-      [x] Escucho click en el botón de enviar
-      [x] Recogemos los datos del input y los guardamos en una variable
-      [x] Hacemos fetch para pedir los datos del servidor con el filtro de búsqueda del usuario
-      [x] Parseo los datos recibidos
-      [x] El servidor me devuelve la info
-      [x] Pinto N resultados en el html
-            [x] Pinto titulo
-            [x] Pinto imagen y en el caso de no tener, poner por defecto 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV?'
-      [x] Usuario elige resultados favoritos y se almacenan en un nuevo array
-      [ ] Cambiamos la clase cambiando fondo y fuente
-      [ ] Pintamos resultados de nuevo
-            [ ] En un div los resultados de la búsqueda
-            [ ] En otro div los favoritos
-      [ ] Quitamos los favoritos si el usuario vuelve a seleccionar el resultado guardado como favorito
-      [ ] Guardamos datos de favoritos en local storage
-      [ ] Se muestran siempre los favoritos aunque se hagan nuevas búsquedas
-      [ ] Comprobar y recuperar de local storage
-
-      [ ] BONUS: Afinar maqutación
+Buscador de series
+Aplicación web realizada con Javascript en la que filtramos series por nombre y las podemos almacenar en favoritos.
+Realizado por Yolanda Vergara
--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,27 +1,21 @@
 'use strict';
-//variable donde voy a mostrar el contenido buscado
 const dataResults = document.querySelector('.js-container');
-//variable donde guardamos la búsqueda del usuario
 const userSearch = document.querySelector('.js-input');
-//variable que ejecuta la busqueda
 const btn = document.querySelector('.js-btn');
-//variable predefinida como array en la que almacenaremos los datos para después mostrarlos
 let films = [];
-//variable predefinida como array en la que guardamos los favoritos
 let favoriteFilms = [];
 
-//guardamos las películas guardadas en el array de favoritos para que las cargue posteriormente, las pasamos a formato JSON
+//Local Storage
 function setLocalStorage() {
   localStorage.setItem("favorite", JSON.stringify(favoriteFilms));
 }
-//definimos la función que nos recuperará el localstorage
+
 function getLocalStorage() {
-  const localStorageFavoriteFilms = JSON.parse(localStorage.getItem("favorite"));
- //en el caso de que al coger el json y parsearlo no esté vacío, lo volcamos en nuestro array de favoritos y lo pintamos en pantalla 
+  const localStorageFavoriteFilms = JSON.parse(localStorage.getItem("films"));
   if (localStorageFavoriteFilms !== null) {
     favoriteFilms = localStorageFavoriteFilms;
-    paintFavorites();
-    //en el caso de que esté vacío, ejecutamos la función que nos trae los datos de búsqueda del servidor
+    paintFilms();
+    listenFilms();
   } else {
     getServerData();
   }
@@ -29,15 +23,10 @@ function getLocalStorage() {
 
 //FETCH
 function getServerData() {
-//hacemos la búsqueda en la API, usando los criterios de búsqueda de la documentación de la misma
-  fetch(
-      `http://api.tvmaze.com/search/shows?q=${userSearchnp.value}`
-    )//al traernos los datos del servidor, los guardamos en un array para poder usar los datos de forma cómoda, posteriormente ejecutamos la función de recuperar el localstorage, lo pintamos en pantalla y ejecutamos la función que escucha las series al clickarlas para posteriormente añadirlas a favoritos
-
+  fetch(`http://api.tvmaze.com/search/shows?q=${userSearch.value}`)
     .then(response => response.json())
     .then(function (serverData) {
       films = serverData;
-      getLocalStorage();
       paintFilms();
       listenFilms();
     })
@@ -46,49 +35,40 @@ function getServerData() {
     });
 }
 
-//FUNCION PINTAR RESULTADOS
+//Mostrar resultados en pantalla
 function paintFilms() {
   let htmlCode = '';
-//creamos un método que recorre los resultados mostrados y almacena en favoritos cen función de si el id se encuentra en el array o no 
   for (let i = 0; i < films.length; i++) {
     const favoriteIndex = favoriteFilms.findIndex(function (show, favoriteIndex) {
       return show.id === films[i].show.id;
     })
-//en el caso de que devuelva la posición del id y sea diferente a -1 lo marca como favodito
     const isFavorite = favoriteIndex !== -1;
-
-    const defaultImage =
-      'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
-//en este condicional añadimos la clase que se le pone en el caso de estar en el array de favoritos
+    const defaultImage = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
     if (isFavorite) {
       htmlCode += `<li class="js-films js-film-item favorite-film" id=${films[i].show.id} >`;
     } else {
       htmlCode += `<li class="js-films js-film-item" id=${films[i].show.id} >`;
     }
     htmlCode += `<h3 class="js-films-name">${films[i].show.name}</h3>`;
-//en este condicional indicamos la imagen que muestra por defecto en caso de no existir
     if (films[i].show.image !== null) {
       let filmImage = films[i].show.image.medium;
       htmlCode += `<img src="${filmImage}">`;
     } else {
-      htmlCode += `<img src="${defaultImage}"`;
+      htmlCode += `<img src="${defaultImage}">`;
     }
     htmlCode += `</li>`;
   }
   dataResults.innerHTML = htmlCode;
 }
 
-
+//Marcar favoritos
 function toogleFavorites(ev) {
-  //metemos en una constante el id de los elementos seleccionados
   const clickedItem = parseInt(ev.currentTarget.id);
-  //metemos en una constante el valor del id que tiene el elemento escuchado, si es igual al id que hemos almacenado en clickedItem añade o elimina en el caso de no serlo del array de favoritos
   const favoriteIndex = favoriteFilms.findIndex(function (show, favoriteIndex) {
     return show.id === clickedItem
   })
 
   const isFavorite = favoriteIndex !== -1;
-
   if (isFavorite === true) {
     favoriteFilms.splice(favoriteIndex, 1);
   } else {
@@ -98,29 +78,26 @@ function toogleFavorites(ev) {
       }
     }
   }
-  //posteriormente repintamos los favoritos, preparamos para otra búsqueda, repintamos los nuevos resultados y los almacenamos en localStorage
   paintFilms();
   listenFilms();
   paintFavorites();
   setLocalStorage();
 }
-
+//Pintar favoritos
 function paintFavorites() {
+  const defaultImage = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
   const containerFav = document.querySelector('.js-paint-favorites');
   let htmlCode = "";
   for (let i = 0; i < favoriteFilms.length; i++) {
     htmlCode += `<li class=js-favorites id='${favoriteFilms[i].id}'>`;
     htmlCode += `<h3 class="favTitle">${favoriteFilms[i].name}</h3>`;
     if (favoriteFilms[i].image === null) {
-      htmlCode +=
-        '<img class="item__img"src="./assets/images/tvPlaceholder.png"/>';
+      htmlCode += `<img src="${defaultImage}">`;
     } else {
       htmlCode += `<img class='item__img'src='${favoriteFilms[i].image.medium}'/>`;
     }
-
     htmlCode += "</li>";
   }
-
   containerFav.innerHTML = htmlCode;
 }
 
@@ -128,10 +105,9 @@ function handler(ev) {
   ev.preventDefault();
   getServerData();
 }
-//definimos la función que va a escuchar todos los resultados de ls búsquedas para añadir a favoritos
+//Función listener
 function listenFilms() {
   const filmItems = document.querySelectorAll('.js-film-item');
-
   for (const filmItem of filmItems) {
     filmItem.addEventListener('click', toogleFavorites);
   }
